cbb8ddc7c58ad54f1ea361618a7f80613299721f
myzhoulang <604389771@qq.com>
2016-02-24 20:56:58 +0800

    add

--- routes/ui.js
+++ routes/ui.js
@@ -1,25 +1,32 @@
 /* GET users listing. */
-exports.dialog = function(req, res) {
-  res.render('dialog', {
-    title: '对话框'
-  });
+exports.dialog = function (req, res) {
+    res.render('dialog', {
+        title: '对话框'
+    });
 };
 
 
-exports.segment = function(req, res) {
-  res.render('segment', {
-    title: '分段选择'
-  })
+exports.segment = function (req, res) {
+    res.render('segment', {
+        title: '分段选择'
+    });
 };
 
-exports.lists = function(req, res) {
-  res.render('lists', {
-    title: 'Lists'
-  })
+exports.lists = function (req, res) {
+    res.render('lists', {
+        title: 'Lists'
+    });
 };
 
-exports.panel = function(req, res) {
-  res.render('panel', {
-    title: 'Panel'
-  })
+exports.panel = function (req, res) {
+    res.render('panel', {
+        title: 'Panel'
+    });
+};
+
+//排版
+exports.typography = function (req, res) {
+    res.render('typography', {
+        title: 'Typography'
+    });
 }
